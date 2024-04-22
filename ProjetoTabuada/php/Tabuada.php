00b1c0af1979324e81e66ab9719b005f93db9c4d
gianluca Home <gianluca.graciolli@gmail.com>
2024-04-22 12:22:49 -0300

    Atualizando atividade Tabuada php

--- ProjetoTabuada/php/Tabuada.php
+++ ProjetoTabuada/php/Tabuada.php
@@ -1,15 +1,13 @@
-<?php 
+<?php
 
-$number = filter_input(INPUT_GET,"numero", FILTER_VALIDADE_NUMBER_INT);
+$number = filter_input(INPUT_GET, "numero", FILTER_VALIDATE_INT);
 
-
-
-
-
-
-
-
-
+if (verifyNumber($number) == null) {
+    echo"<h1> Tabuda do número " . $number . " </h1>";
+    CalculateTable($number);
+} else {
+    header("Location: ./../index.html");
+}
 
 
 
@@ -26,10 +24,29 @@ $number = filter_input(INPUT_GET,"numero", FILTER_VALIDADE_NUMBER_INT);
 
 
 
-function verifyNumber ($number) {
-    if ($number < 0 && !is_int($number)) {
+
+
+
+
+
+
+function CalculateTable($number)
+{
+    $resultado = 0;
+    echo "<table>";
+    echo "<tr> <th> Tabuada </th> <th> Resultado </th>";
+    for ($i = 1; $i <= 10; $i++) {
+        echo "<tr> <td> " . $number . " X " . $i . " </td> <td> " . $resultado = $number * $i . " </td> </tr>";
+    }
+    echo "</table>";
+
+}
+
+
+
+function verifyNumber($number)
+{
+    if ($number < 0 || !is_int($number)) {
         echo "Digite um número válido";
-    } else {
-        return $number;
     }
 }
