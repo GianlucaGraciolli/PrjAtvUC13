4e3e5ea514cf41b3d936a19c1331fd69fcf69e88
Gianluca <gianluca.graciolli@hotmail.com>
2024-04-22 21:21:11 -0300

    commit

--- ProjetoTabuada/php/Tabuada.php
+++ ProjetoTabuada/php/Tabuada.php
@@ -28,8 +28,9 @@ $number = filter_input(INPUT_GET,"numero", FILTER_VALIDADE_NUMBER_INT);
 
 function verifyNumber ($number) {
     if ($number < 0 && !is_int($number)) {
-        echo "Digite um número válido";
-    } else {
-        return $number;
+        return false;
     }
+
+    return true;
+    
 }
